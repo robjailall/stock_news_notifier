218a84d34349c9ce9dcaad4625c7e13436e9fab9
Robert Jailall <rob.jailall@gmail.com>
2020-08-20 13:05:00 -0700

    Turn back on

--- config/news_sources.py
+++ config/news_sources.py
@@ -9,12 +9,12 @@ news_sources = {
             "class": "region-content"
         }
     },
-    # "hhs_warp_speed": {
-    #     "url": "https://search.hhs.gov/searchblox/servlet/SearchServlet?cname=HHS&facet=true&featuredAds=true&page=1&pagesize=10&query=Warp%2520speed%2520contract%2520&xsl=json",
-    #     "format": "json",
-    #     "element_selector": {
-    #         "key_chain": ["results", "result"]
-    #     },
-    #     "redirect_url": "https://search.hhs.gov/searchblox/hhs/index.html?query=Warp%20speed%20contract%20&page=1&pagesize=10&sort=article:modified_time&sortdir=desc&HHS=Search&adsCname=HHS&adsDisplay=true&cname=hhsgov_only&default=AND&tune=true&tune.0=10&tune.1=8&tune.2=2&tune.3=5&tune.4=365&tune.5=30"
-    # }
+    "hhs_warp_speed": {
+        "url": "https://search.hhs.gov/searchblox/servlet/SearchServlet?cname=HHS&facet=true&featuredAds=true&page=1&pagesize=10&query=Warp%2520speed%2520contract%2520&xsl=json",
+        "format": "json",
+        "element_selector": {
+            "key_chain": ["results", "result"]
+        },
+        "redirect_url": "https://search.hhs.gov/searchblox/hhs/index.html?query=Warp%20speed%20contract%20&page=1&pagesize=10&sort=article:modified_time&sortdir=desc&HHS=Search&adsCname=HHS&adsDisplay=true&cname=hhsgov_only&default=AND&tune=true&tune.0=10&tune.1=8&tune.2=2&tune.3=5&tune.4=365&tune.5=30"
+    }
 }
