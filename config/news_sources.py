691f5bd1476a445444825b873b224cb52d7e2a9f
Robert Jailall <rob.jailall@gmail.com>
2020-09-11 13:06:34 -0700

    Turn off dhs

--- config/news_sources.py
+++ config/news_sources.py
@@ -5,12 +5,12 @@ news_sources = {
         "url": lambda: "https://home.treasury.gov/policy-issues/cares/preserving-jobs-for-american-industry/loans-to-air-carriers-eligible-businesses-and-national-security-businesses",
         "element_selector": {"id": "national-loans"}
     },
-    "development_finance_corp_loi": {
-        "url": lambda: "https://www.dfc.gov/search/node?keys=letter%20of%20interest",
-        "element_selector": {
-            "class": "region-content"
-        }
-    },
+    # "development_finance_corp_loi": {
+    #     "url": lambda: "https://www.dfc.gov/search/node?keys=letter%20of%20interest",
+    #     "element_selector": {
+    #         "class": "region-content"
+    #     }
+    # },
     "hhs_warp_speed": {
         "url": lambda: "https://search.hhs.gov/searchblox/servlet/SearchServlet?HHS=Search&adsCname=HHS&adsDisplay=true&cname=hhsgov_only&startdate=3&startdate=0&default=AND&facet=true&page=1&pagesize=30&query=Warp%2520speed%2520contract%2520&tune=true&tune.0=10&tune.1=8&tune.2=2&tune.3=5&tune.4=365&tune.5=30&xsl=json",
         "format": "json",
