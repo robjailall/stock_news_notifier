0add5a611e33c550482bad1ebca61713b8dcafb1
Robert Jailall <rob.jailall@gmail.com>
2020-11-05 21:03:09 -0800

    Turn off HHS

--- config/news_sources.py
+++ config/news_sources.py
@@ -11,16 +11,16 @@ news_sources = {
     #         "class": "region-content"
     #     }
     # },
-    "hhs_warp_speed": {
-        "url": lambda: "https://search.hhs.gov/searchblox/servlet/SearchServlet?HHS=Search&adsCname=HHS&adsDisplay=true&cname=hhsgov_only&startdate=3&startdate=0&default=AND&facet=true&page=1&pagesize=30&query=Warp%2520speed%2520contract%2520&tune=true&tune.0=10&tune.1=8&tune.2=2&tune.3=5&tune.4=365&tune.5=30&xsl=json",
-        "format": "json",
-        "element_selector": {
-            "key_chain": ["results", "result"],
-            "result_key_filter": ["title", "description"]
-        },
-        # "element_selector": {"id": "results_list"},
-        "redirect_url": lambda: "https://tinyurl.com/y3vge6wa"
-    },
+    # "hhs_warp_speed": {
+    #     "url": lambda: "https://search.hhs.gov/searchblox/servlet/SearchServlet?HHS=Search&adsCname=HHS&adsDisplay=true&cname=hhsgov_only&startdate=3&startdate=0&default=AND&facet=true&page=1&pagesize=30&query=Warp%2520speed%2520contract%2520&tune=true&tune.0=10&tune.1=8&tune.2=2&tune.3=5&tune.4=365&tune.5=30&xsl=json",
+    #     "format": "json",
+    #     "element_selector": {
+    #         "key_chain": ["results", "result"],
+    #         "result_key_filter": ["title", "description"]
+    #     },
+    #     # "element_selector": {"id": "results_list"},
+    #     "redirect_url": lambda: "https://tinyurl.com/y3vge6wa"
+    # },
     "spac_filings": {
         #  curl -v -X POST https://efts.sec.gov/LATEST/search-index -d '{"dateRange":"5y","startdt":"2020-07-11","enddt":"2020-07-16","category":"all","locationType":"located","locationCode":"all","forms":["424B4","8K"],"page":"1","from":0}'
         "url": lambda: "https://efts.sec.gov/LATEST/search-index",
